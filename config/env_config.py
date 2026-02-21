3318db8b8d3552947126ef973f1bca8cd1fff0cc
Jazeem <jazeem.abdul.jabbar@gmail.com>
2026-02-21 11:16:41 +0400

    env_config.py updated with wait times

--- config/env_config.py
+++ config/env_config.py
@@ -8,6 +8,9 @@ class EnvConfig:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     BASE_URL = "https://morent-car.archisacademy.com/"
     BROWSER = "chrome"
+    SHORT_WAIT_TIME = 5
+    MEDIUM_WAIT_TIME = 10
+    LONG_WAIT_TIME = 20
 
     def get_config_value(self):
         """Placeholder for config retrieval."""
